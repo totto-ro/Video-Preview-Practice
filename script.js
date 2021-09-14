c306707bd8e483b53b01200167eb6d57f4736c4f
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-09-13 21:08:42 -0600

    JS Changes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("page loaded...");
 
 function play (element) {
-    let catVideo01 = document.getElementById( 'mainVideo' );
+    let catVideoStar = document.getElementById( '.vid-s' ).duration;
+    document.getElementById('.vid-s').innerHTML = catVideoStar;
     
 }
 
 function stop (element) {
-    let catMiniVideo = document.getElementById( 'mainVideo' ).paused;
+    let catVideoPaused = document.getElementById('.vid-s').paused;
+    document.getElementById('.vid-s').innerHTML = catVideoPaused;
 }
-
